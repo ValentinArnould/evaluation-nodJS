c6f6dc1fc772b5095f39c70e94ca95b775a33124
test <test>
2019-10-03 13:22:54 +0200

    on peut ajouter des users

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,46 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var mongo = require('./../bin/mongo');
+var mongo = require("./../bin/mongo");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
   var ctNbr = 0;
   var lastCts = mongo.modele();
-  lastCts.find({}, (err,users) => {
-
+  lastCts.find({}, (err, users) => {
     console.log(users);
     ctNbr = users.length;
-    res.render('index', {contactNbr: ctNbr, 
-      lastContacts: users.reverse()});
+    res.render("index", { contactNbr: ctNbr, lastContacts: users.reverse() });
   });
 });
 
-router.get('/changeContact', function(req, res, next) {
-  var lastCts = mongo.modele();
-  lastCts.findOne({}, (err,user) => {
+router.get("/changeContact/:id", function(req, res, next) {
+  if (req.params.id != "newOne") {
+    var lastCts = mongo.modele();
+    lastCts.findOne({}, (err, user) => {
+      console.log(user);
 
-    console.log(user);
-    
-      res.render('changeContact', {user: user});
-  });
+      res.render("changeContact", { user: user });
+    });
+  } else {
+    res.render("changeContact", {
+      user: {
+        avatar: "",
+        nom: "",
+        prenom: "",
+        description: "",
+        telephones: [{ nom: "", telephone: "" }],
+        emails: [{ nom: "", email: "" }]
+      }
+    });
+  }
 });
 
-router.get('/contact/:id', function(req, res, next) {
+router.get("/contact/:id", function(req, res, next) {
   var lastCts = mongo.modele();
-  lastCts.findOne({_id: req.params.id}, (err,user) => {
-
+  lastCts.findOne({ _id: req.params.id }, (err, user) => {
     console.log(user);
-    
-      res.render('contact', {user: user});
+
+    res.render("contact", { user: user });
   });
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var mongo = require("./../bin/mongo");
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
@@ -7,33 +8,50 @@ router.get("/", function(req, res, next) {
 });
 
 router.post("/", function(req, res, next) {
-  console.log(req.body.telephoneName);
   var telephones = [];
-  req.body.telephoneName.length.forEach(el => {
+  if(typeof(req.body.telephoneName) === 'object') {
+    Object.keys(req.body.telephoneName).forEach(el => {
+      telephones.push({
+        nom: req.body.telephoneName[el],
+        telephone: req.body.telephoneValue[el]
+      });
+    });
+  } else {
     telephones.push({
-      nom: req.body.telephoneName.this[el],
-      telephone: req.body.telephoneValue.this[el]
+      nom: req.body.telephoneName,
+      telephone: req.body.telephoneValue
     });
-  });
-  var emails = [];
-  req.body.telephoneName.length.forEach(el => {
+  }
+    var emails = [];
+  if(typeof(req.body.emailName) === 'object') {
+    Object.keys(req.body.emailName).forEach(el => {
+      emails.push({
+        nom: req.body.emailName[el],
+        email: req.body.emailValue[el]
+      });
+    });
+  } else {
     emails.push({
-      nom: req.body.emailName.this[el],
-      email: req.body.emailValue.this[el]
+      nom: req.body.emailName,
+      email: req.body.emailValue
     });
-  });
+  }
 
   var newContact = {
     avatar: req.body.avatar,
-    nom: req.body,
-    prenom: req.body,
-    description: req.body,
+    nom: req.body.nom,
+    prenom: req.body.prenom,
+    description: req.body.description,
     telephones: telephones,
     emails: emails
   };
+  console.log(newContact);
   var entry = mongo.modele();
   var contact = new entry(newContact);
-  contact.save().then(() => console.log("contact crée"));
+  contact.save().then(() => {
+    console.log("contact crée");
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
